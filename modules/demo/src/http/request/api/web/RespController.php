a89ee62bb917c6c76732fdfca3982de733d08e22
多厘 <zhaody901@126.com>
2022-05-06 15:18:46 +0800

    fix: resp 的链接

--- modules/demo/src/http/request/api/web/RespController.php
+++ modules/demo/src/http/request/api/web/RespController.php
@@ -4,6 +4,8 @@ namespace Demo\Http\Request\Api\Web;
 
 use Poppy\Framework\Application\Controller;
 use Poppy\Framework\Classes\Resp;
+use Poppy\Framework\Validation\Rule;
+use Validator;
 
 class RespController extends Controller
 {
@@ -47,6 +49,32 @@ class RespController extends Controller
     public function error()
     {
         return Resp::error('返回错误提示');
+    }
+
+    /**
+     * @api                    {get} api/demo/resp/validator   [Demo]Resp-Validator
+     * @apiDescription         接口失败请求
+     * @apiVersion             1.0.0
+     * @apiName                RespValidator
+     * @apiGroup               Demo
+     */
+    public function validator()
+    {
+        $validator = Validator::make([
+            'user' => '',
+            'my' => ''
+        ], [
+            'user' => [
+                Rule::required()
+            ],
+            'my' => [
+                Rule::required()
+            ]
+        ]);
+        if ($validator->fails()) {
+            return Resp::error($validator->messages());
+        }
+        return Resp::success('验证通过');
     }
 
     /**
