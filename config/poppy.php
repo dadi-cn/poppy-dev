0f866febce76f365c4bcbc4ad37e1e31a126fe14
多厘 <zhaody901@126.com>
2022-06-06 21:25:33 +0800

    feat: 图片上传的类型约定

--- config/poppy.php
+++ config/poppy.php
@@ -39,7 +39,7 @@ return [
             // common software
             'x-os', 'x-ver', 'x-id', 'x-type',
             // system
-            'x-sys-name', 'x-sys-version', 'x-sys-device', 'x-sys-cpu',
+            'x-sys-name', 'x-sys-version', 'x-sys-device', 'x-sys-cpu', 'x-sys-network',
             // append
             'x-k1', 'x-k2', 'x-k3', 'x-k4', 'x-k5',
             'x-k6', 'x-k7', 'x-k8', 'x-k9', 'x-k10',
