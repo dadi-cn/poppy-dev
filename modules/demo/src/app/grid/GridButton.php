c3d387bdc46443a8f53a625450e1970039b9cf08
多厘 <zhaody901@126.com>
2022-06-28 10:42:58 +0800

    feat: 加入 dialog 模式

--- modules/demo/src/app/grid/GridButton.php
+++ modules/demo/src/app/grid/GridButton.php
@@ -25,6 +25,7 @@ class GridButton extends GridBase
             $actions->request('确认', route('demo:api.grid.request', ['success']))->info()->confirm();
             $actions->copy('复制', '可以复制的内容')->icon('mu:content_copy')->circle()->only();
             $actions->page('Page & Form', route('demo:api.form.auto', ['field-text']), 'form');
+            $actions->dialog('Dialog & Form', route('demo:api.form.auto', ['field-text']), 'form');
             $actions->page('Page & Grid', route('demo:api.grid.auto', ['layout']), 'grid');
             $actions->target('Target(百度)', 'https://www.baidu.com');
             $actions->iframe('Iframe', 'https://poppy-framework.com/');
