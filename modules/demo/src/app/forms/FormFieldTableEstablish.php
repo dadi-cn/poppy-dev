03b9b68e3b220c69b9eed1071989d2f376416041
多厘 <zhaody901@126.com>
2022-06-28 12:02:11 +0800

    feat: table 支持多图预览

--- modules/demo/src/app/forms/FormFieldTableEstablish.php
+++ modules/demo/src/app/forms/FormFieldTableEstablish.php
@@ -4,6 +4,7 @@ namespace Demo\App\Forms;
 
 use Demo\Models\DemoWebapp;
 use Poppy\Framework\Classes\Resp;
+use Poppy\MgrApp\Classes\Table\EzTable;
 use Poppy\MgrApp\Classes\Table\TablePlugin;
 use Poppy\MgrApp\Classes\Widgets\FormWidget;
 
@@ -44,10 +45,10 @@ class FormFieldTableEstablish extends FormWidget
             $table->add('name', '用户名');
             $table->add('action', '操作')->width(160, true)->asTableAction();
         });
-        $this->table('ez-table', '表格')->easy([
-            ['Header', 'Title', 'Value'],
-            ['网站', '地址', 'https://www.baidu.com'],
-            ['网站', '名称', '百度'],
+        $this->ezTable('ez-table', '表格')->easy([
+            ['Header', 'Title', 'Value', '图片'],
+            ['网站', '地址', 'https://www.baidu.com', EzTable::image('https://jdc.jd.com/img/428x280?color=282828&textColor=EAE0D0&text=wulicode')],
+            ['网站', '名称', '百度', EzTable::image('https://start.wulicode.com/img/428x280/wulicode')],
         ]);
     }
 }
